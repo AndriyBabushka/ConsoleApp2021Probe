6cb1f2a1007e941fbf4101d3f79ac6700e098189
AndriyBabushka <andriy.babushka@gmail.com>
2021-07-21 12:16:20 +0300

    Revert "Comm10"
    This reverts commit d2226600ad5384ccaea80f7d8b5c1ecb7b436d6b.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,5 +3,10 @@
 int main()
 {
     std::cout << "Hello World!\n" << std::endl;
-    
+    std::cout << "dfgrfdg\n";
+    std::cout << "gdsfrg\n";
+    std::cout << "gsrdg\n";
+    std::cout << "New1111\n";
+    std::cout << "NEW11111\n";
+    std::cout << "NEW1\n";
 }
